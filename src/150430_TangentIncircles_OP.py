36cb4b018fafb201f4567c71996138dc5973760b
unknown <adel@LAP0371.hlmn.dk>
2015-05-01 12:07:27 +0200

    Added 150430_TangentIncircles example.

---
+++ src/150430_TangentIncircles_OP.py
@@ -0,0 +1,19 @@
+﻿"""
+How to make and output Kangaroo2 goals.
+-
+Author: Anders Holden Deleuran
+Github: github.com/AndersDeleuran/KangarooGHPython
+Updated: 150429
+"""
+
+import clr
+clr.AddReferenceToFile("KangarooSolver.dll")
+import KangarooSolver as ks
+import Rhino as rc
+from System.Collections.Generic import List
+
+# Convert Python points list to .NET type list
+ptsList = List[rc.Geometry.Point3d](Pts)
+
+# Make the OnPlane goal and output to GH
+P = ks.Goals.OnPlane(ptsList,rc.Geometry.Plane.WorldXY,1.0)
---
+++ src/150430_TangentIncircles_TI.py
@@ -0,0 +1,24 @@
+﻿"""
+How to make and output Kangaroo2 goals.
+-
+Author: Anders Holden Deleuran
+Github: github.com/AndersDeleuran/KangarooGHPython
+Updated: 150429
+"""
+
+# Add reference to the Kangaroo dll
+import clr
+clr.AddReferenceToFile("KangarooSolver.dll")
+
+# Import the Kangaroo namespace using an alias
+import KangarooSolver as ks
+
+# Make empty output list
+TI = []
+
+# Iterate inputs and make goals
+for i in range(len(A)):
+    g = ks.Goals.TangentIncircles(A[i],C[i],B[i],D[i],10.0)
+    
+    # Add to output list
+    TI.append(g)
