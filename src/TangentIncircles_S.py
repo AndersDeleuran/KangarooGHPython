5bc3d239b671684d56c1f6cd86b858ec5fd247bf
unknown <adel@LAP0371.hlmn.dk>
2015-05-01 12:41:39 +0200

    Figuring out how to implement ks.Goals.Locator()

---
+++ src/TangentIncircles_S.py
@@ -0,0 +1,21 @@
+﻿"""
+How to make and output Kangaroo2 goals.
+-
+Author: Anders Holden Deleuran
+Github: github.com/AndersDeleuran/KangarooGHPython
+Updated: 150429
+"""
+
+import Grasshopper as gh
+import clr
+clr.AddReferenceToFile("KangarooSolver.dll")
+import KangarooSolver as ks
+
+# Make empty output list
+GL = []
+
+# Iterate inputs and make goals
+for geo in G:
+    gow = gh.Kernel.Types.GH_ObjectWrapper(gh.Kernel.Types.GH_Mesh(geo)) # Must be a more general way of doing this
+    gl = ks.Goals.Locator(gow)
+    GL.append(gl)
